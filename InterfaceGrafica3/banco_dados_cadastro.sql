7a197e24a551467826ab48fb44f62679333e892f
Maria Fernanda Ferreira Mariano <137118092+nandabtw@users.noreply.github.com>
2025-02-08 19:51:54 -0300

    Update banco_dados_cadastro.sql
    Criação da tabela fornecedor.

--- InterfaceGrafica3/banco_dados_cadastro.sql
+++ InterfaceGrafica3/banco_dados_cadastro.sql
@@ -13,6 +13,19 @@ CREATE TABLE cadastro.cadastro_pessoa (
 
 select * from cadastro_pessoa;
 
+CREATE TABLE fornecedor (
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ nome VARCHAR(100) NOT NULL,
+ email VARCHAR(100) NOT NULL,
+ endereco VARCHAR(200) NOT NULL,
+ uf INT NOT NULL,
+ telefone VARCHAR(20) NOT NULL,
+ cnpj VARCHAR(20) NOT NULL,
+ inscricaoEstadual VARCHAR(20) NOT NULL,
+ nomeFantasia VARCHAR(100) NOT NULL,
+ categoria VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE cadastro.uf (
  id    INT auto_increment NOT NULL,
  nome  varchar(100)       NOT NULL,

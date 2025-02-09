6a4e200df8b8e21b341ce222cbf5ae7e3a305c9c
Maria Fernanda Ferreira Mariano <137118092+nandabtw@users.noreply.github.com>
2025-02-08 21:17:19 -0300

    Update Fornecedor.java
    Foram corrigidos bugs apresentados na versão anterior

--- InterfaceGrafica3/src/interfacegrafica3/model/Fornecedor.java
+++ InterfaceGrafica3/src/interfacegrafica3/model/Fornecedor.java
@@ -1,21 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package interfacegrafica3.model;
 
 public class Fornecedor extends PessoaJuridica {
+
+    // Métodos estáticos
+    public static String size() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static Pessoa get(int i) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    // Atributos
     private String categoria;
     private Uf uf;
 
-    public Fornecedor() {
-    }
-
-    public Fornecedor(int id, String nome, String email, String endereco, String telefone, String cnpj,
-            String inscricaoEstadual, String nomeFantasia, String categoria, Uf uf) {
+    // Construtor completo
+    public Fornecedor(int id, String nome, String email, String endereco, String telefone, String cnpj, String inscricaoEstadual, String nomeFantasia, String categoria, Uf uf) {
         super(cnpj, inscricaoEstadual, nomeFantasia, nome, email, endereco, telefone, id);
         this.categoria = categoria;
         this.uf = uf;
+    }
+
+    public Fornecedor(String nome, String email, String cnpj, String endereco, String telefone, String cidade, String estado, int id) {
+    super(nome, email, cnpj, endereco, telefone, cidade, estado, id);
     }
 
     // Getters e Setters
